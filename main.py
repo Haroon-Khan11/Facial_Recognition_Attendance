016d519074b3536d524ba65998836b2da693eb09
Haroon-Khan11 <65364860+Haroon-Khan11@users.noreply.github.com>
2024-05-16 06:37:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ Bootstrap5(app)
 
 @app.route("/")
 def home():
-    return render_template("index.html", role='instructor')
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
